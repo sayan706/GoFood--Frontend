055e5df86e3f27fd44a168e38618ef65aee10160
sayan706 <97615918+sayan706@users.noreply.github.com>
2023-02-24 22:14:51 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -7,7 +7,7 @@ export default function Signup() {
     
     const handleSubmit =async(e)=>{
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/createuser",{
+        const response = await fetch("https://go-food-backend-pfp4r3wv4-sayan706.vercel.app/api/createuser",{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
