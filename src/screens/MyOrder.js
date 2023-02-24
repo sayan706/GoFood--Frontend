8ee06e69840dfe3ccb831031042785c7d382c855
sayan706 <97615918+sayan706@users.noreply.github.com>
2023-02-24 23:08:53 +0530

    Update MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -7,7 +7,7 @@ export default function MyOrder() {
     
         const fetchMyOrder = async () => {
             console.log(localStorage.getItem('userEmail'))
-            await fetch("https://go-food-backend-pfp4r3wv4-sayan706.vercel.app/api/myOrderData", {
+            await fetch("https://go-food-backend.vercel.app/api/myOrderData", {
                 // credentials: 'include',
                 // Origin:"http://localhost:3000/login",
                 method: 'POST',
